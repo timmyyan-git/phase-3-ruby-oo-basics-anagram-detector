9b6c18d4968e55e6cbaaaafcfdf537beee8297f4
Timmy Yan <timmyyan2000@gmail.com>
2022-12-13 16:59:48 -0500

    commit tree 2bf27a66afc2a22100d8c3bb3084e909c8c3f10b

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :name
+
+    def initialize(word)
+        @word = word
+    end
+
+    def matche(array)
+        array.selec {|x| x.split("").sort == @name.split("").sort}
+    end
+end
